aab3bff2a686d514fc73fc59a2cc088de3a1e9ca
Hubert LOBIT <hubert.lobit@gmail.com>
2016-09-22 04:14:59 +0200

    API data fetched.

--- src/app/app.js
+++ src/app/app.js
@@ -7,16 +7,46 @@ import '../style/app.scss';
 let app = ['$timeout', ($timeout) => {
   return {
     template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app',
+    controller: ['$scope', '$q', '$http', ($scope, $q, $http) => {
+      $scope.url = 'https://github.com/hlobit/neth';
+      let defer = $q.defer();
+      $http
+        .get('http://nethapi.apps.noemi-et-hubert.eu/playlists/1', { cache: 'true'})
+        .success(function(data) {
+            defer.resolve(data);
+        });
+
+      defer.promise.then(function(data){
+        let elements = data.elements;
+        $scope.songs = [];
+        for (let i = 0; i < elements.length; i++) {
+          let element = elements[i];
+          let title = element.song.title;
+          if(element.song.artist) {
+            title += ' (' + element.song.artist + ')';
+          }
+          let media = {};
+          for (let j = 0; j < element.song.versions[0].media.length; j++) {
+            media[element.song.versions[0].media[j].role] = element.song.versions[0].media[j].file.path;
+          }
+          $scope.songs.push({
+            type: element.name,
+            title: title,
+            mp3: media['mp3'],
+            m4a: media['m4a'],
+            score: media['main']
+          });
+        }
+      });
+    }],
     link: (scope, elm, attrs) => {
       $timeout(() => {
         plyr.setup({
           controls: ["play", "progress", "current-time", "duration"],
-          tooltips: { "controls": true, "seek": false },
+          tooltips: { "controls": false, "seek": false },
           volume: "10"
         });
-      });
+      }, 1000);
     }
   }
 }];
@@ -26,88 +56,10 @@ let song = () => {
     template: require('./song.html'),
     restrict: 'A',
     controller: ['$scope', ($scope) => {
-      $scope.song.scoreUrl = 'https://docs.google.com/viewerng/viewer?url=http://chants.noemi-et-hubert.eu/scores/' + $scope.song.id + '.pdf';
-      $scope.song.musicUrl = 'music/' + $scope.song.id + '.mp3';
-      $scope.song.musicDownloadUrl = 'music/' + $scope.song.id + '.m4a';
+      $scope.song.scoreUrl = 'https://docs.google.com/viewerng/viewer?url=http://nethapi.apps.noemi-et-hubert.eu' + $scope.song.score;
+      $scope.song.musicUrl = 'http://nethapi.apps.noemi-et-hubert.eu' + $scope.song.mp3;
+      $scope.song.musicDownloadUrl = 'http://nethapi.apps.noemi-et-hubert.eu' + $scope.song.m4a;
     }]
-  }
-}
-
-class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/hlobit/neth';
-    this.songs = [
-      {
-        type: 'Chant d\'entrée',
-        id: 'frappez_dans_vos_mains',
-        title: 'Frappez dans vos mains (Pat Berning)',
-      },
-      {
-        type: 'Gloria',
-        id: 'gloria_slzm',
-        title: 'Messe des saints Louis et Zélie Matin',
-      },
-      {
-        type: 'Alleluia',
-        id: 'alleluia_rio',
-        title: 'JMJ de Rio (JMJ 2013)',
-      },
-      {
-        type: '«Litanie» des saints',
-        id: 'priere_a_tous_les_saints',
-        title: 'Prière à tous les saints',
-      },
-      {
-        type: 'Chant à l\'Esprit',
-        id: 'viens_esprit_de_dieu',
-        title: 'Viens Esprit de Dieu (Renouveau Polonais)',
-      },
-      {
-        type: 'Action de grâce',
-        id: 'les_noces_eternelles',
-        title: 'Les noces éternelles',
-      },
-      {
-        type: 'Prière universelle',
-        id: 'entends_notre_voix',
-        title: 'Entends notre voix',
-      },
-      {
-        type: 'Offertoire',
-        id: 'je_veux_n_etre_qu_a_toi',
-        title: 'Je veux n\'être qu\'à toi (Exo)',
-      },
-      {
-        type: 'Sanctus',
-        id: 'sanctus_slzm',
-        title: 'Messe des saints Louis et Zélie Matin',
-      },
-      {
-        type: 'Anamnèse',
-        id: 'anamnese_slzm',
-        title: 'Messe des saints Louis et Zélie Matin',
-      },
-      {
-        type: 'Agnus Dei',
-        id: 'agnus_slzm',
-        title: 'Messe des saints Louis et Zélie Matin',
-      },
-      {
-        type: 'Chant pendant la communion',
-        id: 'mon_ancre_et_ma_voile',
-        title: 'Mon ancre et ma voile (David Durham)',
-      },
-      {
-        type: 'Chant après la communion',
-        id: 'au_monde',
-        title: 'Au monde (Etincelo)',
-      },
-      {
-        type: 'Chant à Marie',
-        id: 'bienheureuse_marie',
-        title: 'Bienheureuse Marie (Etincelo)',
-      },
-    ];
   }
 }
 
@@ -116,6 +68,10 @@ const MODULE_NAME = 'app';
 angular.module(MODULE_NAME, [])
   .directive('app', app)
   .directive('song', song)
-  .controller('AppCtrl', AppCtrl);
+  .filter('trusted', ['$sce', function ($sce) {
+    return function(url) {
+      return $sce.trustAsResourceUrl(url);
+    };
+  }]);
 
 export default MODULE_NAME;
