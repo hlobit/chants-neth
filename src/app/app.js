b539dcab4d4237025a0471d9239dc9baa047550f
Hubert LOBIT <hubert.lobit@gmail.com>
2017-07-28 01:04:28 +0200

    deployment

--- src/app/app.js
+++ src/app/app.js
@@ -9,35 +9,32 @@ let app = ['$timeout', ($timeout) => {
     template: require('./app.html'),
     controller: ['$scope', '$q', '$http', ($scope, $q, $http) => {
       $scope.url = 'https://github.com/hlobit/neth';
-      let defer = $q.defer();
       $http
         .get('api/playlists/1', { cache: 'true'})
         .then(function(data) {
-            defer.resolve(data);
+          let elements = data.elements;
+          $scope.songs = [];
+          for (let i = 0; i < elements.length; i++) {
+            let element = elements[i];
+            let title = element.song.title;
+            if(element.song.artist) {
+              title += ' (' + element.song.artist + ')';
+            }
+            let media = {};
+            for (let j = 0; j < element.song.versions[0].media.length; j++) {
+              media[element.song.versions[0].media[j].role] = element.song.versions[0].media[j].file.path;
+            }
+            $scope.songs.push({
+              type: element.name,
+              title: title,
+              mp3: media['mp3'],
+              m4a: media['m4a'],
+              score: media['main']
+            });
+          }
+        }).catch(function(err) {
+          console.error(err);
         });
-
-      defer.promise.then(function(data){
-        let elements = data.elements;
-        $scope.songs = [];
-        for (let i = 0; i < elements.length; i++) {
-          let element = elements[i];
-          let title = element.song.title;
-          if(element.song.artist) {
-            title += ' (' + element.song.artist + ')';
-          }
-          let media = {};
-          for (let j = 0; j < element.song.versions[0].media.length; j++) {
-            media[element.song.versions[0].media[j].role] = element.song.versions[0].media[j].file.path;
-          }
-          $scope.songs.push({
-            type: element.name,
-            title: title,
-            mp3: media['mp3'],
-            m4a: media['m4a'],
-            score: media['main']
-          });
-        }
-      });
     }],
     link: (scope, elm, attrs) => {
       $timeout(() => {
