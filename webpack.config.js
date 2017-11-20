65e2acde63c995ff0cd209669d7484eeb835e749
Hubert LOBIT <hubert.lobit@gmail.com>
2017-11-20 19:42:52 +0100

    added a polyfill service / removed webpack deprecation warnings

--- webpack.config.js
+++ webpack.config.js
@@ -198,13 +198,9 @@ module.exports = function makeWebpackConfig() {
   // Add build specific plugins
   if (isProd) {
     config.plugins.push(
-      // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+      // Reference: https://webpack.js.org/plugins/no-emit-on-errors-plugin/
       // Only emit files when there are no errors
-      new webpack.NoErrorsPlugin(),
-
-      // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
-      // Dedupe modules in the output
-      new webpack.optimize.DedupePlugin(),
+      new webpack.NoEmitOnErrorsPlugin(),
 
       // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
       // Minify all javascript, switch loaders to minimizing mode
